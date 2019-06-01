81ece6c46b7c306ba5576a95bd0e274068883421
lalitha-05 <43853822+lalitha-05@users.noreply.github.com>
2019-06-01 14:10:23 +0530

    Create main.php

---
+++ main.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<!-- your webpage info goes here -->
+
+    <title>TheBelievers.com</title>
+	
+	<meta name="author" content="your name" />
+	<meta name="description" content="" />
+
+<!-- you should always add your stylesheet (css) in the head tag so that it starts loading before the page html is being displayed -->	
+	<link rel="stylesheet" href="style.css" type="text/css" />
+<style>
+body{
+background-image: url("women.jpg ");
+background-repeat:no-repeat;
+background-size:1300px 650px;
+color:black
+}
+</style>
+	
+</head>
+<body>
+
+<!-- webpage content goes here in the body -->
+
+	<div id="page">
+		<div id="logo">
+			<h1><a href="/" id="logoLink">The Believers</a></h1>
+		</div>
+		<div id="nav">
+			<ul>
+				<li><a href="user_sign.php">user sign up</a></li>
+				<li><a href="user_login.php">login</a></li>
+				<li><a href="tour_women.php">Take tour</a></li>
+			</ul>	
+		</div>
+<div id="content">
+			<h2>Our Motto</h2>
+			<p>
+				Shout Out to all Women out there...<BR><BR>
+Financially not stable..<BR><BR>
+Here it is to full fill your dreams!!
+			</p>
+</div>
+</body>
+</html>
